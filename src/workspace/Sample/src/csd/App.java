8d83049c04f6b0498baf978c6e156d6860e587e2
Oğuz Karan <oguzkaran@csystem.org>
2021-08-28 10:40:05 +0300

    Java-Aug-2021

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,12 +1,36 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Java 9 ile birlikte alttire karakteri tek başına değişken ismi olarak kullanılamaz
+	Bir metodun geri dönüş değeri return deyimi ile oluşturulur. return deyiminin genel biçimi:
+	
+	return [ifade];
+	
+	return deyiminin temel görevi metodu sonlandırmaktır. return deyimine ilişkin ifadenin değeri metodu çağıran
+	metoda aktarılır
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
-	public static void main(String [] args)
-	{		
+	public static void main(String [] args) 
+	{
+		int result;
 		
+		result = Sample.foo() * 2;
+		
+		System.out.println(result);
 	}
 }
+
+class Sample {
+	public static int foo()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		System.out.print("Bir sayı giriniz:");
+		int val = Integer.parseInt(kb.nextLine());
+		
+		int result = val * val;
+		
+		return result;				
+	}
+}
+
 
