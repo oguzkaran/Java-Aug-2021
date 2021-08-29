71156fd56cac7e8aa696fec3a57e76a53aa97fa5
Oğuz Karan <oguzkaran@csystem.org>
2021-08-29 10:44:39 +0300

    Update

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,28 +1,15 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Bir metodun parametrelerine değerler çağrılırken aktarılır. Metot çağrısında bir parametreye verilen ifadeye 
-	"argüman (argument)" denir. Argümanlardan parametrelere yapılan aktarımda bir atama işlemi olarak düşünülmelidir
+	System.out.printf metodu  ile geçilen argümanın türü ile yer tutucunun ilişkin olduğu tür farklı ise genel olarak
+	exception oluşur. Bu durumun istisnaları vardır
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{		
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("Birinci sayıyı giriniz:");
-		int a = Integer.parseInt(kb.nextLine());
+	{
+		double a = 0.1, b = 0.2;
 		
-		System.out.print("İkinci sayıyı giriniz:");
-		double b = Double.parseDouble(kb.nextLine());
-		
-		Sample.foo(a, b);
+		System.out.format("%.20f%n", a + b);
 	}
 }
 
-class Sample {
-	public static void foo(int a, double b)
-	{		
-		System.out.println(a);
-		System.out.println(b);
-	}	
-	
-}
