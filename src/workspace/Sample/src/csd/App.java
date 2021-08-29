081d6a3258aa5f1eddd4372f3384cedbfc0debc7
Oğuz Karan <oguzkaran@csystem.org>
2021-08-29 12:35:09 +0300

    Update

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,15 +1,18 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	System.out.printf metodu  ile geçilen argümanın türü ile yer tutucunun ilişkin olduğu tür farklı ise genel olarak
-	exception oluşur. Bu durumun istisnaları vardır
+	Math sınıfının logaritmik işlem yapan metotları
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
-		double a = 0.1, b = 0.2;
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("Bir sayı giriniz:");
+		double val = Double.parseDouble(kb.nextLine());
 		
-		System.out.format("%.20f%n", a + b);
+		System.out.printf("log(%.2f) = %f%n", val, Math.log(val));		
+		System.out.printf("log10(%.2f) = %f%n", val, Math.log10(val));
+		System.out.printf("log1p(%.2f + 1) = %f%n", val, Math.log1p(val));		
 	}
 }
 
