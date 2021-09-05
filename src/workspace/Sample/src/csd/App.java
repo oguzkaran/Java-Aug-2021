fa3d9cd4cff9ea372aef74e1824008521969e4c7
Oğuz Karan <oguzkaran@csystem.org>
2021-09-05 12:30:16 +0300

    Update

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,12 +1,48 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Aşağıdaki yol bilgisinin String atomu olarak yazımı
-	\\192.168.1.123\test\names.txt
+	Operatörler 3(üç) biçimde sınıflandırılabilir:
+	1. İşlevlerine göre sınıflandırma
+		- Aritmetik (arithmetic) operatörler
+		- Karşılaştırma (comparison) operatörleri
+		- Mantıksal (logical) operatörler
+		- Bitsel (bitwise) operatörler
+		- Özel amaçlı (special purpose) operatörler
+	2. Operand sayısına göre sınıflandırma
+		- Tek operandlı (unary)
+		- İki operandlı (binary)
+		- Üç operandlı (ternary)
+	3. Operatörün konumuna göre sınıflandırma
+		- Önek operatörler (prefix)
+		- Araek operatörler (infix)
+		- Sonek operatörler (postfix)
+		
+	Operatörün kısıtı (constraint)
+	Operatörün ürettiği değer (product value)
+	Operatörün yan etkisi (side effect) var mı?
+	Operatörün önceliği (precedence):
+		a = b + c * d ifadesinin hesaplanması
+		
+		i1: c * d
+		i2: b + i1
+		i3: a = i2
+		
+		a = (b + c) * d ifadesinin hesaplanması
+		
+		i1: b + c
+		i2: i2 * d
+		i3: a = i2
+		
+		a = b + c - d ifadesinin hesaplanması
+		
+		i1: b + c
+		i2: i1 - d
+		i3: a = i2		
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{		
+	{	
 		
+					
 	}
 }
