077fbee4707f6690a723eb6e5cc830de48c46413
Oğuz Karan <oguzkaran@csystem.org>
2021-09-05 10:36:56 +0300

    Update

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,18 +1,12 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Math sınıfının logaritmik işlem yapan metotları
+	Aşağıdaki yol bilgisinin String atomu olarak yazımı
+	\\192.168.1.123\test\names.txt
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("Bir sayı giriniz:");
-		double val = Double.parseDouble(kb.nextLine());
+	{		
 		
-		System.out.printf("log(%.2f) = %f%n", val, Math.log(val));		
-		System.out.printf("log10(%.2f) = %f%n", val, Math.log10(val));
-		System.out.printf("log1p(%.2f + 1) = %f%n", val, Math.log1p(val));		
 	}
 }
-
