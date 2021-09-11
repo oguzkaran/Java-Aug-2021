5a8496022d75836b2edb002fbeee7bb7e3465aef
Oğuz Karan <oguzkaran@csystem.org>
2021-09-11 12:50:41 +0300

    Update

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,11 +1,16 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	-- operatörünün prefix kullanımında ürettiği değer (işleme giren değer) azaltılMAmış değerdir 
+	Aşağıdaki örnekte atama operatörünün birinci operandının değişken olmamasından dolayı error oluşur
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{	
+		int a = 45, b = 23, c = 34;
 		
+		a = b = c = 10; //error
+		
+		System.out.printf("a = %d, b = %d, c = %d%n", a, b, c);
 	}
 }
+
