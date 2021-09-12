4c57c8ef54af14f462aa859ca1eb3a2c689b4267
Oğuz Karan <oguzkaran@csystem.org>
2021-09-12 12:36:10 +0300

    Update

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,28 +1,51 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	 isEven metodunun en okunabilir yazılış şekli aşağıdaki gibidir 
+	 
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{	
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("Bir sayı giriniz:");
-		int val = Integer.parseInt(kb.nextLine());
-		
-		if (NumberUtil.isEven(val))
-			System.out.println("Çift sayı girdiniz");
-		else
-			System.out.println("Tek sayı girdiniz");
-		
-		System.out.println("Tekrar yapıyor musunuz?");		
+		EquationSolverApp.run();		
 	}
 }
 
-class NumberUtil {
-	public static boolean isEven(int val)
+
+class EquationSolverApp {
+	public static void run()
 	{
-		return val % 2 == 0;
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("a?");
+		double a = Double.parseDouble(kb.nextLine());
+		
+		System.out.print("b?");
+		double b = Double.parseDouble(kb.nextLine());
+		
+		System.out.print("c?");
+		double c = Double.parseDouble(kb.nextLine());
+		
+		EquationSolver.findRoots(a, b, c);		
 	}
 }
 
+class EquationSolver {
+	public static double getDeltaValue(double a, double b, double c)
+	{
+		return b * b - 4 * a * c;
+	}
+	
+	public static void findRoots(double a, double b, double c)
+	{
+		double delta = getDeltaValue(a, b, c);
+		
+		if (delta >= 0) {
+			double sqrtDelta = Math.sqrt(delta); 
+			double x1 = (-b + sqrtDelta) / (2 * a);
+			double x2 = (-b - sqrtDelta) / (2 * a);
+			
+			System.out.printf("x1 = %f, x2 = %f%n", x1, x2);			
+		}		
+		else
+			System.out.println("Gerçek kök yok");
+	}
+}
