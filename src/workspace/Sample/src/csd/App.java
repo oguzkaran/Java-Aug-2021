f0785a21dc5fc0ce933e65319ed30416a70b71ed
Oğuz Karan <oguzkaran@csystem.org>
2021-10-02 10:32:37 +0300

    Update

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,45 +1,31 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Sınıf Çalışması: Parmetresi ile aldığı bir sayının factorial değerini döndüren factorial isimli metodu yazınız ve
-	aşağıdaki kod ile test ediniz.
-	0! = 1
-	1! = 1
-	2! = 1 * 2
-	3! = 1 * 2 * 3
-	
-	...
-	n! = 1 * 2 * 3 * ... * (n- 1) * n
+	short, byte ve char türlerine "sınırlar içerisinde kalması koşuluyla int türden bir sabitin doğrudan atanabilmesi"
+	kuralı metodun çağrısında argümanlardan parametrelere yapılan atama işleminde geçersizdir 
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{		
-		FactorialTest.run();
+		Sample.foo(10); //error
+		Sample.bar(100); //error
+		Sample.tar(67); //error
 	}
 }
 
-class FactorialTest {
-	public static void run()
+class Sample {
+	public static void foo(short a)
 	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("Bir sayı giriniz:");
-		int n = Integer.parseInt(kb.nextLine());
-		
-		for (int i = 0; i <= n; ++i)
-			System.out.printf("%d! = %d%n", i, NumberUtil.factorial(i));		
-		
-		System.out.println("Tekrar yapıyor musunuz?");
+		//...
 	}
-}
-
-class NumberUtil {
-	public static int factorial(int n)
+	
+	public static void bar(byte a)
 	{
-		int result = 1;
-		
-		for (int i = 2; i <= n; ++i)
-			result *= i;
-		
-		return result;
+		//...
+	}
+	
+	public static void tar(char a)
+	{
+		//...
 	}
 }
