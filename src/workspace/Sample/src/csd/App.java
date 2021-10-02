70aebeb53a40b0a8e018040be3785eef0f8bc283
Oğuz Karan <oguzkaran@csystem.org>
2021-10-02 12:40:03 +0300

    Update

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,31 +1,15 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	short, byte ve char türlerine "sınırlar içerisinde kalması koşuluyla int türden bir sabitin doğrudan atanabilmesi"
-	kuralı metodun çağrısında argümanlardan parametrelere yapılan atama işleminde geçersizdir 
+	Herhangi bir türden boolean türüne explicit dönüşüm geçersizdir
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{		
-		Sample.foo(10); //error
-		Sample.bar(100); //error
-		Sample.tar(67); //error
+	{	
+		int a = 1;
+		boolean b;
+		
+		b = (boolean)a; //error
 	}
 }
 
-class Sample {
-	public static void foo(short a)
-	{
-		//...
-	}
-	
-	public static void bar(byte a)
-	{
-		//...
-	}
-	
-	public static void tar(char a)
-	{
-		//...
-	}
-}
