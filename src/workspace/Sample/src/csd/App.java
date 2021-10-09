21b94830aeaba28ca7f475f37aae29f12e950955
Oğuz Karan <oguzkaran@csystem.org>
2021-10-09 12:27:36 +0300

    Update

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,64 +1,51 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	 1. Aday metotlar: 1, 2, 3, 4, 5, 6, 7, 8
-	 2. Uygun metotlar: 4, 8
-	 3. En uygun metot: 4 
+	 Aşağıdaki örnekte Math sınıfının abs metotları simüle edilmiştir. Kodu çalıştırarak hangi metodun hangi durumda
+	 çağrıldığını gözlemleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{		
-		float a = 10.3F;
-		short b = 345;
+		{
+			int i; //
+			int a;
+			
+			
+		}
+		i = 10;
+		a = 10;
+	}
+}
+
+class MyMath {
+	public static double abs(double a)
+	{
+		System.out.println("abs, double");
 		
-		Sample.foo(a, b); //#4
+		return a < 0 ? -a : a;	
 	}
+	
+	public static float abs(float a)
+	{
+		System.out.println("abs, float");
+		
+		return a < 0 ? -a : a;	
+	}
+	
+	public static long abs(long a)
+	{
+		System.out.println("abs, long");
+		
+		return a < 0 ? -a : a;
+	}
+	
+	public static int abs(int a)
+	{
+		System.out.println("abs, int");
+		
+		return a < 0 ? -a : a;
+	}
+	
+	//...	
 }
-
-class Sample {
-	public static void foo() //#1
-	{
-		System.out.println("foo");
-	}
-	
-	public static void foo(int a) //#2
-	{
-		System.out.println("foo, int");
-	}
-	
-	public static void foo(int a, int b) //#3
-	{
-		System.out.println("foo, int, int");	
-	}
-
-	public static void foo(double a, int b) //#4
-	{
-		System.out.println("foo, double, int");	
-	}
-	
-	public static void foo(int a, long b) //#5
-	{
-		System.out.println("foo, int, long");	
-	}
-	
-	public static void foo(int a, double b) //#6
-	{
-		System.out.println("foo, int, double");	
-	}
-	
-	public static void foo(int a, char b) //#7
-	{
-		System.out.println("foo, int, char");	
-	}
-	
-	public static void foo(double a, long b) //#8
-	{
-		System.out.println("foo, double, long");	
-	}
-	
-	public static void bar(double a) //#9
-	{
-		System.out.println("bar, double");
-	}
-}
-
