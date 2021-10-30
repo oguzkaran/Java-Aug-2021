dfe315ea66bd6237dead0f8a6b06ee1e206c534b
Oğuz Karan <oguzkaran@csystem.org>
2021-10-30 11:00:36 +0300

    Update

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,17 +1,46 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	 Sınıf Çalışması: 
+	 Sınıf Çalışması: Parametresi ile aldığı iki yazıdan birincisi içerisinde ikincisinden kaç tane olduğunu döndüren
+	 countString isimli metodu StringUtil sınıfı içerisinde yazınız ve aşağıdaki kod ile test ediniz.
+	 Örnek: aaa yazısı içerisinde aa dan 2 tane var	    
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
-		String s = "Bugün hava bulutlu. Yağmur yağabilir. Yağmur yağma ihtimaline karşılık şemsiye alınız";
-		String str = "Yağmur";
-		
-		int index = s.lastIndexOf(str);		
-		
-		System.out.printf("Index:%d%n", index);		
+		CountStringTest.run();
 	}	
 }
 
+
+class CountStringTest {	
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		for (;;) {
+			System.out.printf("Birinci yazıyı giriniz:");
+			String s1 = kb.nextLine();
+			
+			if ("elma".equals(s1))
+				break;
+			
+			System.out.printf("İkinci yazıyı giriniz:");
+			String s2 = kb.nextLine();
+			int count = StringUtil.countString(s1, s2);
+			
+			System.out.printf("Sayı:%d%n", count);
+		}
+		
+		System.out.println("Tekrar yapıyor musunuz?");
+		
+		
+	}	
+}
+
+class StringUtil {
+	public static int countString(String s1, String s2)
+	{
+		//TODO:
+	}
+}
