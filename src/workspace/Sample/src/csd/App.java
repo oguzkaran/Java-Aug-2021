45d9a2620818b9f95ba9e99385ff0ccf7a0e2278
Oğuz Karan <oguzkaran@csystem.org>
2021-10-31 10:30:45 +0300

    Update

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,18 +1,54 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Sınıf Çalışması: Klavyeden elma girilene kadar alınan yazıların arasında - karakteri olacak şekilde birleştirilmiş
-	bir String oluşturan programı yazınız.
-	Örneğin: ankara istanbul izmir zonguldak girildiğinde
-	ankara-istanbul-izmir-zonguldak
-	yazısı elde edilecektir
+	Character sınıfının yararlı bazı toxxx metotları
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
-		String s = "ankara";
-		
-		System.out.println(s.substring(6, 6).isEmpty() ? "Boş string" : "Boş string değil");		
+		UpperLowerConvertApp.run();
 	}	
+}
+
+class UpperLowerConvertApp {
+	public static char getRandomLetter(java.util.Random r)
+	{
+		String lettersTR = "çğışöüÇĞIİŞÖÜ";
+		
+		int val = r.nextInt(3);
+		
+		char c;
+		
+		switch (val) {
+		case 0:
+			c = (char)(r.nextInt(26) + 'A');
+			break;			
+		case 1:
+			c = (char)(r.nextInt(26) + 'a');
+			break;
+		default:
+			c = lettersTR.charAt(r.nextInt(lettersTR.length()));
+		}
+		
+		return c;
+	}
+	
+	public static void run()
+	{
+		java.util.Random r = new java.util.Random();
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("Bir sayı giriniz:");
+		int n = Integer.parseInt(kb.nextLine());
+		
+		for (int i = 0; i < n; ++i) {
+			char ch = getRandomLetter(r);
+			
+			System.out.println("-------------------------");
+			System.out.printf("[%d] -> %c -> ", (int)ch, ch);
+			System.out.println(Character.isUpperCase(ch) ? Character.toLowerCase(ch) : Character.toUpperCase(ch));
+			System.out.println("-------------------------");
+		}	
+		
+	}
 }
 
