26df9eafb8769b95e7409fa70540821a28d54c6f
Oğuz Karan <oguzkaran@csystem.org>
2021-10-31 09:50:16 +0300

    Update

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,54 +1,18 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Sınıf Çalışması: Kalvyeden bir kullanıcı adı ve şifre isteyen basit bir ATM uygulamasının b ir parçası olan 
-	programı aşağıdaki açıklamalara göre yazınız:	
-	Açıklamalar:
-	- Kullanıcı adı ve şifre 3(üç) kez denenebilecektir.
-	- 3(üç) kez deneme sırasında doğru giriş yapılırsa "Giriş başarılı", 3(üç) kez deneme de yanlış olursa 
-	"Giriş başarısız. Deneme hakkınız bitti!..." mesajı verilecektir. 
-	- Kullanıcı adı ve şifrenin doğruluk kontrolü program içerisinde belirlenen bir kullanıcı ve şifre ile yapılacaktır
-	- Programı genel düşünerek yazınız
-	
-	Not:Program şu ana kadar gördüklerimiz ile yazılmıştır	
-	 
+	Sınıf Çalışması: Klavyeden elma girilene kadar alınan yazıların arasında - karakteri olacak şekilde birleştirilmiş
+	bir String oluşturan programı yazınız.
+	Örneğin: ankara istanbul izmir zonguldak girildiğinde
+	ankara-istanbul-izmir-zonguldak
+	yazısı elde edilecektir
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
-		ReverseTest.run();
+		String s = "ankara";
+		
+		System.out.println(s.substring(6, 6).isEmpty() ? "Boş string" : "Boş string değil");		
 	}	
 }
 
-class ReverseTest {
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		for (;;) {
-			System.out.printf("Bir yazı giriniz:");
-			String s = kb.nextLine();
-			
-			if ("elma".equals(s))
-				break;
-			
-			System.out.println("---------------------------");
-			System.out.println(StringUtil.reversed(s));
-			System.out.println("---------------------------");
-		}
-		
-		System.out.println("Tekrar yapıyor musunuz?");
-	}
-}
-
-class StringUtil {
-	public static String reversed(String s)
-	{
-		String rev = "";
-		
-		for (int i = s.length() - 1; i >= 0; --i)
-			rev += s.charAt(i);	
-		
-		return rev;
-	}
-}
