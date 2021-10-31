ada9c599c43974f944d34b0c6bedfd00c5791f92
Oğuz Karan <oguzkaran@csystem.org>
2021-10-31 11:00:37 +0300

    Update

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,54 +1,43 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Character sınıfının yararlı bazı toxxx metotları
+	Sınıf Çalışması: Parametresi ile aldığı bir yazının ilk harfi büyük geri kalanları küçük olacak şekilde bir 
+	yazı döndüren capitalize isimli metodu yazınız ve aşağıdaki kod ile test ediniz.
+	Örneğin: yazı "bugün HAVA çok Güzel" ise "Bugün hava çok güzel" yazısına geri dönecektir  
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
-		UpperLowerConvertApp.run();
+		CapitalizeTest.run();
 	}	
 }
 
-class UpperLowerConvertApp {
-	public static char getRandomLetter(java.util.Random r)
-	{
-		String lettersTR = "çğışöüÇĞIİŞÖÜ";
-		
-		int val = r.nextInt(3);
-		
-		char c;
-		
-		switch (val) {
-		case 0:
-			c = (char)(r.nextInt(26) + 'A');
-			break;			
-		case 1:
-			c = (char)(r.nextInt(26) + 'a');
-			break;
-		default:
-			c = lettersTR.charAt(r.nextInt(lettersTR.length()));
-		}
-		
-		return c;
-	}
-	
+class CapitalizeTest {	
 	public static void run()
-	{
-		java.util.Random r = new java.util.Random();
+	{		
 		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("Bir sayı giriniz:");
-		int n = Integer.parseInt(kb.nextLine());
 		
-		for (int i = 0; i < n; ++i) {
-			char ch = getRandomLetter(r);
+		for (;;) {
+			System.out.print("Bir yazı giriniz:");
+			String s = kb.nextLine();
 			
-			System.out.println("-------------------------");
-			System.out.printf("[%d] -> %c -> ", (int)ch, ch);
-			System.out.println(Character.isUpperCase(ch) ? Character.toLowerCase(ch) : Character.toUpperCase(ch));
-			System.out.println("-------------------------");
+			System.out.println("--------------------------");
+			System.out.printf("(%s)%n", s);
+			System.out.printf("(%s)%n", StringUtil.capitalize(s));
+			System.out.println("--------------------------");
+			
+			if ("elma".equals(s))
+				break;
 		}	
 		
+		System.out.println("Tekrar yapıyor musunuz?");		
+	}
+}
+
+class StringUtil {
+	public static String capitalize(String s)
+	{
+		//TODO:
 	}
 }
 
