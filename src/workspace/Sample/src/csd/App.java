651416b6ec7d02ccea9b9ef9732e3215dcf63636
Oğuz Karan <oguzkaran@csystem.org>
2021-10-31 12:27:00 +0300

    Update

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,77 +1,65 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Sınıf Çalışması: Aşağıda açıklanan metotları StringUtil sınıfı içerisinde yazınız ve aşağıdaki kod ile test ediniz
-		public static String padLeading(String s, int length, char ch);
-		public static String padLeading(String s, int length);
-		public static String padTrailing(String s, int length, char ch);
-		public static String padTrailing(String s, int length);
-	Açıklamalar:
-	- padLeading metodunun 3(üç) parametreli overload'u ikinci parmetresi ile aldığı length uzunluğu yazıdan büyükse 
-	yazının soldan ch karakteri ile doldurulmuş olduğu yeni bir String referansına döndecektir. Örneğin:
-		padLeading("ankara", 8, 'x'); çağrısı
-		xxankara
-	yazısına ilişkin String referansına geri dönecektir
-	
-	 - length yazının uzunluğundan küçük ya da eşit ise aynı referansa geri dönecektir.
-	 
-	 - padLeading metodunun iki parametreli overload'u yazıyı soldan space karakteri ile dolduracaktır
-	 
-	 - padTrailing metotları sağdan dolduracak şekilde padLeading metotlarına benzer biçimde çalışacaktır 
-	 
-	 - Metotları Java 11 öncesi için yazınız
+	Point sınıfı 
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
-		PadLeadingTrailingTest.run();		
+		Point p = new Point(100, 100);
+		
+		System.out.println(p.toString());
 	}	
 }
 
-class PadLeadingTrailingTest {
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);	
-		
-		for (;;) {
-			System.out.printf("Bir yazı giriniz:");
-			String s = kb.nextLine();			
-			
-			if ("elma".equals(s))
-				break;
-			
-			System.out.printf("Uzunluk giriniz:");
-			int length = Integer.parseInt(kb.nextLine());
-			
-			System.out.println("--------------------------------");
-			System.out.printf("(%s)%n", s);
-			System.out.printf("(%s)%n", StringUtil.padLeading(s, length, 'x'));
-			System.out.printf("(%s)%n", StringUtil.padLeading(s, length));
-			System.out.printf("(%s)%n", StringUtil.padTrailing(s, length, 'x'));
-			System.out.printf("(%s)%n", StringUtil.padTrailing(s, length));
-			System.out.println("--------------------------------");
-		}	
-	}
-}
 
-class StringUtil {
-	public static String padLeading(String s, int length, char ch)
-	{
-		
+class Point {
+	public int x;
+	public int y;
+	
+	public Point()
+	{		
 	}
 	
-	public static String padLeading(String s, int length)
+	public Point(int a)
 	{
-		
+		x = a;
 	}
 	
-	public static String padTrailing(String s, int length, char ch)
+	public Point(int a, int b)
 	{
-		
+		x = a;
+		y = b;
 	}
 	
-	public static String padTrailing(String s, int length)
+	public double distance()
 	{
-		
+		return distance(0, 0);
+	}
+	
+	public double distance(Point other)
+	{
+		return distance(other.x, other.y);
+	}
+	
+	public double distance(int a, int b)
+	{
+		return Math.sqrt(Math.pow(x - a, 2) + Math.pow(y - b, 2));
+	}
+	
+	public void offset(int dxy)
+	{
+		offset(dxy, dxy);
+	}
+	
+	public void offset(int dx, int dy)
+	{
+		x += dx;
+		y += dy;
+	}
+	
+	public String toString()
+	{
+		return String.format("{x: %d, y: %d}", x, y);		
 	}
 }
