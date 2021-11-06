dd8a28bd27dade4030ff1dc7c57904c9a5aff905
Oğuz Karan <oguzkaran@csystem.org>
2021-11-06 12:24:20 +0300

    Update

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,23 +1,20 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Sınıf Çalışması: Parametresi ile aldığı bir yazının palndrome olup olmadığını test eden isPalindrome isimli metodu
-	yazınız ve aşağıdaki kod ile test ediniz:
-	Palindrom: Yalnızca alfabetik karakterleri ters ve düz olarak okunduğunda aynı olan yazılara denir. Alfabetik karakterler 
-	dışındakiler yok varsayılır. Örneğin:
-	Ey Edip Adana'da pide ye 	-> eyedipadanadapideye
-	Anastas mum satsana			-> anastasmumsatsana
-	Ali Papila					-> alipapila		  
+	Sınıf Çalışması: Parametresi ile aldığı bir yazının içerisindeki en uzun palindromu döndüren getLongestPalindrome
+	metodunu yazınız ve aşağıdaki kod ile test ediniz
+	
+		abcdadaeyedipadanadapideyeadaecealipapilaanastasmumsatsanaxyzt
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
-		IsPalindromeTest.run();
+		GetLongestPalindromeTest.run();
 	}	
 }
 
 
-class IsPalindromeTest {
+class GetLongestPalindromeTest {
 	public static void run()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
@@ -29,7 +26,7 @@ class IsPalindromeTest {
 			if ("elma".equals(s))
 				break;
 			
-			System.out.println(StringUtil.isPalindrome(s) ? "Palindrom" : "Palindrom değil");
+			System.out.printf("En uzun palindrom:%s%n", StringUtil.getLongestPalindrome(s));
 		}
 		
 		System.out.println("Tekrar yapıyor musunuz?");
@@ -37,8 +34,58 @@ class IsPalindromeTest {
 }
 
 class StringUtil {
+	public static String getLongestPalindrome(String s)
+	{
+		String maxStr = "";
+		int right = s.length();
+		
+		while (right != 0) {
+			int left = 0;
+			
+			while (left != right) {
+				String str = s.substring(left++, right); 
+				
+				if (str.length() > 1 && isPalindrome(str) && str.length() > maxStr.length())
+					maxStr = str;
+			}
+						
+			--right;
+		}
+		
+		return maxStr;
+	}
+	
 	public static boolean isPalindrome(String s)
 	{
-		//TODO:
+		int left = 0;
+		int right = s.length() - 1;
+		
+		while (left < right) {
+			char cLeft = s.charAt(left);
+			
+			if (!Character.isLetter(cLeft)) {
+				++left;
+				continue;
+			}			
+			
+			cLeft = Character.toLowerCase(cLeft);
+			
+			char cRight = s.charAt(right);
+			
+			if (!Character.isLetter(cRight)) {
+				--right;
+				continue;
+			}
+			
+			cRight = Character.toLowerCase(cRight);
+			
+			if (cLeft != cRight)
+				return false;
+			
+			++left;
+			--right;
+		}
+		
+		return true;
 	}
 }
