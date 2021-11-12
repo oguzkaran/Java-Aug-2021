ad14bc09fb26721b5889b39c39dddced45c6c1f3
Oğuz Karan <oguzkaran@csystem.org>
2021-11-12 09:13:21 +0300

    Update

--- src/workspace/Sample/src/org/csystem/util/numeric/NumberUtil.java
+++ src/workspace/Sample/src/org/csystem/util/numeric/NumberUtil.java
@@ -11,28 +11,340 @@
 package org.csystem.util.numeric;
 
 public class NumberUtil {
-	//...
-	public static boolean isPrime(long val)	
-	{
-		if (val <= 1)
-			return false;
-		
-		if (val % 2 == 0)
-			return val == 2;
-		
-		if (val % 3 == 0)
-			return val == 3;
-		
-		if (val % 5 == 0)
-			return val == 5;
-		
-		if (val % 7 == 0)
-			return val == 7;
-		
-		for (long i = 11; i * i <= val; i += 2)	
-			if (val % i == 0)
-				return false;
-		
-		return true;
-	}		
+
+    public static int calculateDigitalRoot(int val)
+    {
+        int root = Math.abs(val);
+
+        while (root > 9)
+            root = sumDigits(root);
+
+        return root;
+    }
+
+    public static int calculateDigitalRoot(int val)
+    {
+        int root = Math.abs(val);
+
+        while ((root = sumDigits(root)) > 9)
+            ;
+
+        return root;
+    }
+
+    public static int countDigits(int val)
+    {
+        if (val == 0)
+            return 1;
+
+        return (int) Math.log10(Math.abs(val)) + 1;
+    }
+
+    public static void displayCollatz(int n)
+    {
+        if (n <= 0) {
+            System.out.println("Geçersiz sayı");
+            return;
+        }
+
+        System.out.println(n);
+
+        while (n != 1)
+            System.out.println(n = n % 2 == 0 ? n / 2 : 3 * n + 1);
+    }
+
+    public static long factorial(int n)
+    {
+        long result = 1;
+
+        for (long i = 2; i <= n; ++i)
+            result *= i;
+
+        return result;
+    }
+
+    public static int getDigitsFactorialSum(int n)
+    {
+        int total = 0;
+
+        while (n != 0) {
+            total += factorial(n % 10);
+            n /= 10;
+        }
+
+        return total;
+    }
+
+    public static int getDigitsPowSum(int val)
+    {
+        int n = countDigits(val);
+        int result = 0;
+
+        while (val != 0) {
+            result += pow(val % 10, n);
+            val /= 10;
+        }
+
+        return result;
+    }
+
+    public static int getFibonacciNumber(int n)
+    {
+        if (n <= 2)
+            return n - 1;
+
+        int prev1 = 1, prev2 = 0, val = 0;
+
+        for (int i = 2; i < n; ++i) {
+            val = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = val;
+        }
+
+        return val;
+    }
+
+    public static int getIndexOfPrime(long n)
+    {
+        int idx = 1;
+        int val = 2;
+
+        for (;;) {
+            if (val == n)
+                return idx;
+
+            if (isPrime(val))
+                ++idx;
+
+            ++val;
+        }
+    }
+
+    public static int getNextFibonacciNumber(int val)
+    {
+        if (val < 0)
+            return 0;
+
+        int prev1 = 1, prev2 = 0, result;
+
+        for (;;) {
+            result = prev1 + prev2;
+
+            if (result > val)
+                return result;
+
+            prev2 = prev1;
+            prev1 = result;
+        }
+    }
+
+    public static int getPrime(int n)
+    {
+        int count = 0;
+        int val = 2;
+
+        for (int i = 2; count < n; ++i)
+            if (isPrime(i)) {
+                ++count;
+                val = i;
+            }
+
+        return val;
+    }
+
+    public static long getRandomLongPrime(java.util.Random r)
+    {
+        long prime;
+
+        while (!isPrime(prime = r.nextLong()))
+            ;
+
+        return prime;
+    }
+
+    public static boolean isArmstrong(int val)
+    {
+        return val >= 0 && getDigitsPowSum(val) == val;
+    }
+
+    public static boolean isDecimalHarshad(int val)
+    {
+        return val >= 0 && val % sumDigits(val) == 0;
+    }
+
+    public static boolean isEven(int val)
+    {
+        return val % 2 == 0;
+    }
+
+    public static boolean isFactorian(int n)
+    {
+        return n > 0 && getDigitsFactorialSum(n) == n;
+    }
+
+    public static boolean isHardyRamanujan(int val)
+    {
+        if (val <= 0)
+            return false;
+
+        int count = 0;
+
+        EXIT_LOOP:
+        for (int x = 1; x * x * x < val; ++x)
+            for (int y = x + 1; x * x * x + y * y * y <= val; ++y)
+                if (x * x * x + y * y * y == val) {
+                    if (++count == 2)
+                        break EXIT_LOOP;
+                    ++x;
+                }
+
+        return count == 2;
+    }
+
+    public static boolean isOdd(int val)
+    {
+        return !isEven(val);
+    }
+
+    public static boolean isPrime(long val)
+    {
+        if (val <= 1)
+            return false;
+
+        if (val % 2 == 0)
+            return val == 2;
+
+        if (val % 3 == 0)
+            return val == 3;
+
+        if (val % 5 == 0)
+            return val == 5;
+
+        if (val % 7 == 0)
+            return val == 7;
+
+        for (long i = 11; i * i <= val; i += 2)
+            if (val % i == 0)
+                return false;
+
+        return true;
+    }
+
+    public static boolean isPrimeX(long val)
+    {
+        long sum = val;
+        boolean result;
+
+        while ((result = isPrime(sum)) && sum > 9)
+            sum = sumDigits(sum);
+
+        return result;
+    }
+
+    public static boolean isPrimeX(long val)
+    {
+        boolean result;
+
+        for (long sum = val; (result = isPrime(sum)) && sum > 9; sum = sumDigits(sum))
+            ;
+
+        return result;
+    }
+
+    public static boolean isSuperPrime(long val)
+    {
+        return isPrime(val) && isPrime(getIndexOfPrime(val));
+    }
+
+    public static int mid(int a, int b, int c)
+    {
+        if (a <= b && b <= c || c <= b && b <= a)
+            return b;
+
+        if (b <= a && a <= c || c <= a && a <= b)
+            return a;
+
+        return c;
+    }
+
+    public static int min(int a, int b, int c)
+    {
+        return (a < b) ? (a < c ? a : c) : (b < c ? b : c);
+    }
+
+    public static int pow(int a, int b)
+    {
+        int result = 1;
+
+        for (int i = 0; i < b; ++i)
+            result *= a;
+
+        return result;
+    }
+
+    public static void printGoldbachPrimePairs(int val)
+    {
+        for (int a = 2; a < val; ++a) {
+            int b = val - a;
+
+            if (isPrime(a) && isPrime(b) && a <= b)
+                System.out.printf("%d + %d = %d == %d%n", b, a, a + b, val);
+        }
+    }
+
+    public static void printPrimeFactors(int n)
+    {
+        if (n == 0)
+            return;
+
+        n = Math.abs(n);
+
+        int i = 2;
+
+        while (n != 1)
+            if (n % i == 0) {
+                System.out.printf("%d ", i);
+                n /= i;
+            }
+            else
+                ++i;
+
+        System.out.println();
+    }
+
+    public static int reversed(int val)
+    {
+        int result = 0;
+
+        while (val != 0) {
+            result = result * 10 + val % 10;
+            val /= 10;
+        }
+
+        return result;
+    }
+
+    public static int signum(int a)
+    {
+        int result = -1;
+        if (a > 0)
+            result = 1;
+        else if (a == 0)
+            result = 0;
+
+        return result;
+    }
+
+    public static int sumDigits(long val)
+    {
+        int sum = 0;
+
+        while (val != 0) {
+            sum += val % 10;
+            val /= 10;
+        }
+
+        return Math.abs(sum);
+    }
+
 }
--- src/workspace/Sample/src/org/csystem/util/string/StringUtil.java
+++ src/workspace/Sample/src/org/csystem/util/string/StringUtil.java
@@ -6,10 +6,199 @@
 	Utility class that is used for string operations
 
 	Copyleft (c) 1993 by C and System Programmers Association (CSD)
-	All Rights Free
+	All Rights Free	
 -----------------------------------------------------------------------*/
 package org.csystem.util.string;
 
 public class StringUtil {
-	//...
+
+    public static String capitalize(String s)
+    {
+        if (s.isBlank())
+            return s;
+
+        return Character.toUpperCase(s.charAt(0)) + s.substring(1).toLowerCase();
+    }
+
+    public static int countString(String s1, String s2)
+    {
+        int count = 0;
+
+        for (int index = -1; (index = s1.indexOf(s2, index + 1)) != -1; ++count)
+            ;
+
+        return count;
+    }
+
+    public static String getLongestPalindrome(String s)
+    {
+        String result = "";
+
+        int endIndex = s.length();
+
+        while (endIndex != 0) {
+            int beginIndex = 0;
+
+            while (beginIndex != endIndex) {
+                String str = s.substring(beginIndex++, endIndex);
+
+                if (str.length() > 1 && isPalindrome(str) && str.length() > result.length())
+                    result = str;
+            }
+
+            --endIndex;
+        }
+
+        return result;
+    }
+
+    public static String getRandomText(java.util.Random r, int n, String sourceText)
+    {
+        String str = "";
+        int length = sourceText.length();
+
+        for (int i = 0; i < n; ++i)
+            str += sourceText.charAt(r.nextInt(length));
+
+        return str;
+    }
+
+    public static String getRandomTextTR(java.util.Random r, int n)
+    {
+        return getRandomText(r, n, "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZabcçdefgğhıijklmnoöprsştuüvyz");
+    }
+
+    public static String getRandomTextTR(int n)
+    {
+        return getRandomTextTR(new java.util.Random(), n);
+    }
+
+    public static String getRandomTextEN(java.util.Random r, int n)
+    {
+        return getRandomText(r, n, "ABCDEFGHIJKLMNOPQRSTUWXVYZabcdefghijklmnopqrstuwxvyz");
+    }
+
+    public static String getRandomTextEN(int n)
+    {
+        return getRandomTextEN(new java.util.Random(), n);
+    }
+
+    public static boolean isAllLetter(String s)
+    {
+        int length = s.length();
+
+        for (int i = 0; i < length; ++i)
+            if (!Character.isLetter(s.charAt(i)))
+                return false;
+
+        return true;
+    }
+
+    public static boolean isPalindrome(String s)
+    {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            char chLeft = Character.toLowerCase(s.charAt(left));
+
+            if (!isLetter(chLeft)) {
+                ++left;
+                continue;
+            }
+
+            char chRight = Character.toLowerCase(s.charAt(right));
+
+            if (!Character.isLetter(chRight)) {
+                --right;
+                continue;
+            }
+
+            if (chLeft != chRight)
+                return false;
+
+            ++left;
+            --right;
+        }
+
+        return true;
+    }
+
+    public static boolean isPangramTR(String text)
+    {
+        return isPangram(text.toLowerCase(), "abcçdefgğhıijklmnoöprsştuüvyz");
+    }
+
+    public static boolean isPangramEN(String text)
+    {
+        return isPangram(text.toLowerCase(), "abcdefghijklmnopqrstuwxvyz");
+    }
+
+    public static boolean isPangram(String text, String alphabet)
+    {
+        int length = alphabet.length();
+
+        for (int i = 0; i < length; ++i)
+            if (!text.contains(alphabet.charAt(i) + ""))
+                return false;
+
+        return true;
+    }
+
+    public static String padLeading(String s, int length, char ch)
+    {
+        if (length <= s.length())
+            return s;
+
+        return (ch + "").repeat(length - s.length()) + s;
+    }
+
+    public static String padLeading(String s, int length)
+    {
+        return padLeading(s, length, ' ');
+    }
+
+    public static String padTrailing(String s, int length, char ch)
+    {
+        if (length <= s.length())
+            return s;
+
+        return s + (ch + "").repeat(length - s.length());
+    }
+
+    public static String padTrailing(String s, int length)
+    {
+        return padTrailing(s, length, ' ');
+    }
+
+    public static String reverse(String s)
+    {
+        String rev = "";
+
+        for (int i = s.length() - 1; i >= 0; --i)
+            rev += s.charAt(i);
+
+        return rev;
+    }
+
+    public static String trimLeading(String s)
+    {
+        int i;
+        int length = s.length();
+
+        for (i = 0; i < length && Character.isWhitespace(s.charAt(i)); ++i)
+            ;
+
+        return s.substring(i);
+    }
+
+    public static String trimTrailing(String s)
+    {
+        int i;
+
+        for (i = s.length() - 1; i >= 0 && Character.isWhitespace(s.charAt(i)); --i)
+            ;
+
+        return s.substring(0, i + 1);
+    }
 }
