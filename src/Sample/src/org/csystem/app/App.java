f7538469d3b9d3299c944f7435d5c0abc0b41bd2
Oğuz Karan <oguzkaran@csystem.org>
2021-11-14 10:32:02 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,18 +1,21 @@
-/*----------------------------------------------------------------------
-	FILE        : StringUtil.java
-	AUTHOR      : Java-Aug-2021 Group
-	LAST UPDATE : 13.11.2021
-
-	Utility class that is used for string operations
-
-	Copyleft (c) 1993 by C and System Programmers Association (CSD)
-	All Rights Free
------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------------------------------------------------
+    Diziye ilkdeğer olarak verilen elemanlar sabit ifadesi olmak zorunda değildir
+----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import java.util.Scanner;
 
 class App {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World");
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz:");
+        int val = Integer.parseInt(kb.nextLine());
+        int [] a = {val, val + 1, val + 3, 3 * val + 2,};
+
+        for (int i = 0; i < a.length; ++i)
+            System.out.printf("%d ", a[i]);
+
+        System.out.println();
     }
 }
