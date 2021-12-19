b648c0fcf93d2ca4675e9dac9819736486966f02
Oğuz Karan <oguzkaran@csystem.org>
2021-12-19 10:38:27 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,13 +1,20 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Sınıf elemanlarının temel erişim belirleyicileri:
+    Sınıfların özellikle veri elemanlarının gizlenmesine (yani başka bir sınıftan erişilemez duruma getirilmesine)
+    nesne yönelimli programlama tekniğinde "kapsülleme (encapsulation)" denir. Bu kavrama ayrıca
+    "veri/bilgi gizleme (data/information hiding)" de denir. Burada temel amaç dışarıyı yani sınıfın dışını ilgilendirmeyecek
+    verilerin ve bilgilerin gizlenmesidir. Bu kavram gerçek hayattan programlamaya aktarılmıştır. Örneğin araba kullanırken
+    sürücü, vites değiştirdiğinde ya da arabayı kullanırken vites otmatik değiştiğinde bu işlemin içsel olarak nasıl
+    yapıldığını bilmek zorunda değildir. Bilse bile bunun araba kullanırken bir anlamı olmaz.
+
+    Bu anlamda sınıfı yazan (server) ve sınıfı kullanan (client) bakış açısı farklıdır. Şüphesiz sınıfı yazan kişi için iki
+    bakış açısı da düşünülmelidir. Sınıfı kullanan bakış açısı ise (müşteri kodlar) içsel detayları bilmek zorunluluğu
+    getirmez. Kısaca müşteri kodlar kullanımı ilgilendirmeyecek detaylardan bağımsızdır.
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
-
-import org.csystem.application.commandprompt.CommandPromptApp;
 
 class App {
     public static void main(String [] args)
     {
-        CommandPromptApp.run();
+
     }
 }
