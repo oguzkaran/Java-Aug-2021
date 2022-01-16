339af5872aaa0039f53f3197580388d8e39658db
Oğuz Karan <oguzkaran@csystem.org>
2022-01-16 10:36:06 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,15 +1,70 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    MutableFraction sınıfı
+    Aşağıdaki örnekte argümanlar açısından hız ve okunabilirlik durumu çözülebilmiştir. Ancak hala setColor ve move
+    metotlarının parametreleri geçilecek değerler açısından bir fikir vermemektedir
+
+    Örnekte detaylar gözardı edilmiştir
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
-
-import org.csystem.util.math.MutableFraction;
 
 class App {
     public static void main(String [] args)
     {
-        MutableFraction fraction = new MutableFraction(3, 0);
+        SpaceGame game = new SpaceGame();
 
-        System.out.println(fraction.toString());
+        game.run();
     }
+}
+
+class Color {
+    //...
+    public static final int RED = 0;
+    public static final int GREEN = 1;
+    public static final int BLUE = 2;
+    public static final int WHITE = 3;
+    public static final int BLACK = 4;
+}
+
+class Direction {
+    //...
+    public static final int RIGHT = 0;
+    public static final int TOP = 1;
+    public static final int LEFT = 2;
+    public static final int BOTTOM = 3;
+}
+
+class SpaceGame {
+    //...
+    public void run()
+    {
+        Alien alien = new Alien(/*...*/);
+
+        //...
+
+        alien.setColor(Color.RED);
+
+        //...
+        alien.move(Direction.LEFT);
+        alien.move(Direction.TOP);
+        alien.move(Direction.BOTTOM);
+        //...
+    }
+}
+
+class Alien {
+    private int m_color;
+    //...
+    public void setColor(int color)
+    {
+        //...
+        m_color = color;
+    }
+
+    //...
+
+    public void move(int direction)
+    {
+        //...
+    }
+
+    //...
 }
--- src/Sample/src/org/csystem/util/math/MutableFraction.java
+++ src/Sample/src/org/csystem/util/math/MutableFraction.java
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------
 	FILE        : MutableFraction.java
 	AUTHOR      : Java-Aug-2021 Group
-	LAST UPDATE : 15.01.2022
+	LAST UPDATE : 16.01.2022
 
 	MutableFraction class that represents fraction in mathematics
 
@@ -13,6 +13,26 @@ package org.csystem.util.math;
 public class MutableFraction {
     private int m_a;
     private int m_b;
+
+    private static MutableFraction add(int a1, int b1, int a2, int b2)
+    {
+        return new MutableFraction(a1 * b2 + a2 * b1, b1 * b2);
+    }
+
+    private static MutableFraction subtract(int a1, int b1, int a2, int b2)
+    {
+        return add(a1, b1, -a2, b2);
+    }
+
+    private static MutableFraction multiply(int a1, int b1, int a2, int b2)
+    {
+        return new MutableFraction(a1 * a2, b1 * b2);
+    }
+
+    private static MutableFraction divide(int a1, int b1, int a2, int b2)
+    {
+        return multiply(a1, b1, b2, a2);
+    }
 
     private static void check(int a, int b)
     {
@@ -77,11 +97,17 @@ public class MutableFraction {
         set(a, b);
     }
 
-    //...
-
     public int getNumerator()
     {
         return m_a;
+    }
+
+    public void setNumerator(int val)
+    {
+        if (m_a == val)
+            return;
+
+        set(val, m_b);
     }
 
     public int getDenominator()
@@ -89,13 +115,107 @@ public class MutableFraction {
         return m_b;
     }
 
+    public void setDenominator(int val)
+    {
+        if (m_b == val)
+            return;
+
+        check(m_a, val);
+        set(m_a, val);
+    }
+
     public double getRealValue()
     {
         return (double)m_a / m_b;
     }
 
+    //add methods
+    public static MutableFraction add(int val, MutableFraction f)
+    {
+        return add(val, 1, f.m_a, f.m_b);
+    }
+
+    public MutableFraction add(MutableFraction other)
+    {
+        return add(m_a, m_b, other.m_a, other.m_b);
+    }
+
+    public MutableFraction add(int val)
+    {
+        return add(m_a, m_b, val, 1);
+    }
+
+    //subtract methods
+    public static MutableFraction subtract(int val, MutableFraction f)
+    {
+        return subtract(val, 1, f.m_a, f.m_b);
+    }
+
+    public MutableFraction subtract(MutableFraction other)
+    {
+        return subtract(m_a, m_b, other.m_a, other.m_b);
+    }
+
+    public MutableFraction subtract(int val)
+    {
+        return subtract(m_a, m_b, val, 1);
+    }
+
+    //multiply methods
+    public static MutableFraction multiply(int val, MutableFraction f)
+    {
+        return multiply(val, 1, f.m_a, f.m_b);
+    }
+
+    public MutableFraction multiply(MutableFraction other)
+    {
+        return multiply(m_a, m_b, other.m_a, other.m_b);
+    }
+
+    public MutableFraction multiply(int val)
+    {
+        return multiply(m_a, m_b, val, 1);
+    }
+
+    //divide methods
+    public static MutableFraction divide(int val, MutableFraction f)
+    {
+        return divide(val, 1, f.m_a, f.m_b);
+    }
+
+    public MutableFraction divide(MutableFraction other)
+    {
+        return divide(m_a, m_b, other.m_a, other.m_b);
+    }
+
+    public MutableFraction divide(int val)
+    {
+        return divide(m_a, m_b, val, 1);
+    }
+
+    //inc methods
+    public void inc(int val)
+    {
+        m_a += m_b * val;
+    }
+
+    public void inc()
+    {
+        inc(1);
+    }
+
+    //dec methods
+    public void dec(int val)
+    {
+        inc(-val);
+    }
+
+    public void dec()
+    {
+        dec(1);
+    }
     public String toString()
     {
-        return String.format("%d / %d = %f", m_a, m_b, getRealValue());
+        return String.format("%d%s", m_a, m_b == 1 ? "" : " / " + m_b + " = " + getRealValue());
     }
 }
