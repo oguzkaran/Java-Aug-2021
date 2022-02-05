3104f032826b4f976888114c2d627f47b05946ab
Oğuz Karan <oguzkaran@csystem.org>
2022-02-05 12:25:22 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,33 +1,112 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Taban sınıf türünden bir referans türemiş sınıf türünden bir referansa doğrudan atanamaz. Bu işlem tür dönüştürme
-    operatörü ile (explicit conversion, cast) yapılabilir. Bu kavrama "downcasting" denir. Burada tür dönüştürme
-    operatörü derlemeden geçmek için kullanılır.
+    Sarmalayan Sınıflar (wrapper classes): Java'da her temel türe karşılık gelen bir sınıf vardır. Bu sınıflara sarmalayan
+    sınıflar denir. Böyle bir tasarımın nedeni sınıfları tanıdıkça anlaşılacaktır. Bu sınıflarda sarmalamanın dışında
+    ilgili türe ilişkin yararlı işlemler yapan static metotlar da bulunur. Örneğin, Integer sınıfının parseInt metodu
+    bir yazıdan int türden bir değer elde etmek için kullanılır. Temel türlere karşılık gelen sınıflar aşağıdaki
+    tabloda gösterilmektedir:
+
+    Temel Tür               Sarmalayan Sınıfı
+    ------------------------------------------
+    short                       Short
+    int                         Integer
+    long                        Long
+    byte                        Byte
+    double                      Double
+    float                       Float
+    char                        Character
+    boolean                     Boolean
+
+    Bu sınıflardan tamsayı ve gerçek sayı türlerine ilişkin olanlarına (Short, Integer, Long, Byte, Double ve Float)
+    mumerik sarmalayan sınıflar da denebilir.
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import java.util.Random;
+import java.util.Scanner;
 
 class App {
     public static void main(String [] args)
     {
-        int [] a = new int[10];
-        double [] b = new double[20];
-        boolean [] c = new boolean[30];
-        char [] d = new char[10];
-        String [] e = new String[20];
-        Sample [] f = new Sample[40];
-        int [][] g = new int[12][];
-        int [][][] h = new int[12][][];
+        Scanner kb = new Scanner(System.in);
+        AFactory factory = new AFactory();
+        A x;
 
-        System.out.println(a.getClass().getName());
-        System.out.println(b.getClass().getName());
-        System.out.println(c.getClass().getName());
-        System.out.println(d.getClass().getName());
-        System.out.println(e.getClass().getName());
-        System.out.println(f.getClass().getName());
-        System.out.println(g.getClass().getName());
-        System.out.println(h.getClass().getName());
+        System.out.print("Bir sayı giriniz:");
+        int count = Integer.parseInt(kb.nextLine());
+
+        while (count-- > 0) {
+            System.out.println("-----------------------------------------");
+            x = factory.getRandomA();
+            System.out.println(x.getClass().getName());
+            if (x instanceof B) {
+                B y = (B)x;
+
+                y.foo();
+            }
+            else
+                System.out.println("B veya B'den türetilmiş bir tür değil");
+
+            System.out.println("-----------------------------------------");
+        }
+
+        System.out.println("Tekrar yapıyor musunuz?");
     }
 }
 
-class Sample {
+class AFactory {
+    private final Random m_random = new Random();
+
+    //...
+    public A getRandomA()
+    {
+        int val = m_random.nextInt(5);
+        A x;
+
+        switch (val) {
+            case 0:
+                x = new B();
+                break;
+            case 1:
+                x = new C();
+                break;
+            case 2:
+                x = new D();
+                break;
+            case 3:
+                x = new F();
+                break;
+            default:
+                x = new E();
+        }
+
+        return x;
+    }
+}
+
+class F extends D {
+    //...
+}
+
+class E extends B {
+    //...
+}
+
+class D extends A {
+    //...
+}
+
+class C extends B {
+    //...
+}
+
+class B extends A {
+    //...
+    public void foo()
+    {
+        System.out.println("B.foo");
+    }
+}
+
+class A {
     //...
 }
